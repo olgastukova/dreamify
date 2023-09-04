41ea0826d65a019e8213608567b2ae2ed43f4b5f
olgastukova <stukova.oa@gmail.com>
2023-09-04 14:55:29 -0400

    connect ai component to the backend

--- src/components/ChatBot/ChatBot.js
+++ src/components/ChatBot/ChatBot.js
@@ -1,30 +1,48 @@
 
 import React from 'react';
 import crossicon from '../../assets/icons/cross icon.jpg'
+import { useState } from 'react';
+import axios from 'axios';
 
-const ChatBot = props => {
+
+const ChatBot = () => {
+
+const [prompt, updatePrompt] = useState(undefined);
+const [answer, setAnswer] = useState(undefined);
+
+const askAi = (e) => {
+    e.preventDefault();
+
+    axios
+      .post("http://localhost:8080/ask", {
+        prompt: prompt
+      })
+      .then((res) => {
+        console.log('ai result ' + res.data.content);
+        setAnswer(res.data.content);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
     return (
-<section className={`adddream ${props.show ? 'show' : ''}`} onClick={props.onClose}>
-    <div className="adddream__main" onClick={e => e.stopPropagation()}>
-    <button className="adddream__main-button" onClick={props.onClose}>
-        <img className="adddream__main-cross" src={crossicon} alt="white x" />
-    </button>
-    
-    <form className="adddream__main-title">
-        <label>Name</label>
-        <input type="text" name="name"/>
-    </form>
-    <form className="adddream__main-title">
-        <label>Description</label>
-        <input type="text" name="name"/>
-    </form>
-    
-    <div className="adddream__buttons">
-        <button>Add a dream</button>
-    </div>
-    </div>
+/* <section className={`adddream ${props.show ? 'show' : ''}`} onClick={props.onClose}> */
+<section>
+<div className="chatbot__wrapper">
+          <input
+          onChange={(e) => updatePrompt(e.target.value)}
+            type="text"
+            className="chatbot__input"
+            placeholder="Ask me anything..."
+          />
+          <div> 
+            <output className="chatbot__answer" value={answer}>{answer}</output>
+            
+          </div>
+          <button onClick={askAi}>Search</button>
+        </div>
 </section>
     )
 }
- export default ChatBot
+ export {ChatBot};
